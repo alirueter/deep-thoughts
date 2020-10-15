c44e529b25a413cbc1a1757fc945fd1a11154d16
alirueter <rueter.alison@gmail.com>
2020-10-14 20:16:32 -0500

    setup react router

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,19 +8,19 @@ const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
   console.log(thoughts);
-
+  
   return (
     <main>
-  <div className="flex-row justify-space-between">
-    <div className="col-12 mb-3">
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-      )}
-    </div>
-  </div>
-</main>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
+        </div>
+      </div>
+    </main>
   );
 };
 
